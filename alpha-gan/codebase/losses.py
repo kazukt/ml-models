111358f987ab4164ccc7b12d703a20842d2c9ffa
kazukt <kazuki.060525@gmail.com>
2018-06-04 15:56:58 +0900

    ok

---
+++ alpha-gan/codebase/losses.py
@@ -0,0 +1,312 @@
+import functools
+
+import tensorflow as tf
+
+def minimax_discriminator_loss(
+    discriminator_real_outputs,
+    discriminator_gen_outputs,
+    real_weights=1.0,
+    generated_weights=1.0,
+    label_smoothing=0,
+    scope=None,
+    loss_collection=tf.GraphKeys.LOSSES,
+    reduction=tf.losses.Reduction.SUM_BY_NONZERO_WEIGHTS,
+    add_summaries=False):
+    with tf.name_scope(scope, 'discriminator_minimax_loss', [
+        discriminator_gen_outputs, discriminator_real_outputs,
+        real_weights, generated_weights, label_smoothing]) as scope:
+
+        # - log(D(x))
+        loss_on_real = tf.losses.sigmoid_cross_entropy(
+            tf.ones_like(discriminator_real_outputs),
+            discriminator_real_outputs,
+            real_weights,
+            label_smoothing,
+            scope,
+            loss_collection=None,
+            reduction=reduction)
+
+        # - log(1 - D(G(z)))
+        loss_on_fake = tf.losses.sigmoid_cross_entropy(
+            tf.zeros_like(discriminator_gen_outputs),
+            discriminator_gen_outputs,
+            generated_weights,
+            scope=scope,
+            loss_collection=None,
+            reduction=reduction)
+
+        loss = loss_on_real + loss_on_fake
+        tf.losses.add_loss(loss, loss_collection)
+
+        if add_summaries:
+            tf.summary.scalar('discriminator_real_minimax_loss', loss_on_real)
+            tf.summary.scalar('discriminator_gen_minimax_loss', loss_on_fake)
+            tf.summary.scalar('discriminator_minimax_loss', loss)
+
+    return loss
+
+def minimax_generator_loss(
+    discriminator_gen_outputs,
+    weights=1.0,
+    label_smoothing=0,
+    scope=None,
+    loss_collection=tf.GraphKeys.LOSSES,
+    reduction=tf.losses.Reduction.SUM_BY_NONZERO_WEIGHTS,
+    add_summaries=False):
+    with tf.name_scope(scope, 'discriminator_generator_loss', [
+        discriminator_gen_outputs, weights, label_smoothing]) as scope:
+        loss = - minimax_discriminator_loss(
+            tf.ones_like(discriminator_gen_outputs),
+            discriminator_gen_outputs,
+            weights,
+            weights,
+            label_smoothing,
+            scope,
+            loss_collection,
+            reduction,
+            add_summaries=False)
+
+        if add_summaries:
+            tf.summary.scalar('generator_minimax_loss', loss)
+
+    return loss
+
+def modified_discriminator_loss(
+    discriminator_real_outputs,
+    discriminator_gen_outputs,
+    real_weights=1.0,
+    generated_weights=1.0,
+    label_smoothing=0,
+    scope=None,
+    loss_collection=tf.GraphKeys.LOSSES,
+    reduction=tf.losses.Reduction.SUM_BY_NONZERO_WEIGHTS,
+    add_summaries=False):
+    return minimax_discriminator_loss(
+        discriminator_real_outputs,
+        discriminator_gen_outputs,
+        real_weights,
+        generated_weights,
+        label_smoothing,
+        scope or 'discriminator_modified_loss',
+        loss_collection,
+        reduction,
+        add_summaries)
+
+def modified_generator_loss(
+    discriminator_gen_outputs,
+    weights=1.0,
+    label_smoothing=0,
+    scope=None,
+    loss_collection=tf.GraphKeys.LOSSES,
+    reduction=tf.losses.Reduction.SUM_BY_NONZERO_WEIGHTS,
+    add_summaries=False):
+    with tf.name_scope(
+        scope,
+        'generator_modified_loss',
+        [discriminator_gen_outputs, weights, label_smoothing]) as scope:
+        loss = tf.losses.sigmoid_cross_entropy(
+            tf.ones_like(discriminator_gen_outputs),
+            discriminator_gen_outputs,
+            weights,
+            label_smoothing,
+            scope,
+            loss_collection,
+            reduction)
+
+        if add_summaries:
+            tf.summary.scalar('generator_modified_loss', loss)
+
+    return loss
+
+def density_ratio_loss(
+    discriminator_outputs,
+    weights=1.0,
+    label_smoothing=0,
+    scope=None,
+    loss_collection=tf.GraphKeys.LOSSES,
+    reduction=tf.losses.Reduction.SUM_BY_NONZERO_WEIGHTS,
+    add_summaries=False):
+    """Original density ratio loss.
+    """
+    with tf.name_scope(
+        scope, 'density_ratio_loss',
+        [discriminator_outputs, weights, label_smoothing]) as scope:
+
+        loss_class0 = tf.losses.sigmoid_cross_entropy(
+            tf.ones_like(discriminator_outputs),
+            discriminator_outputs,
+            weights,
+            label_smoothing,
+            scope,
+            loss_collection=None,
+            reduction=reduction)
+
+        loss_class1 = - tf.losses.sigmoid_cross_entropy(
+            tf.zeros_like(discriminator_outputs),
+            discriminator_outputs,
+            weights,
+            scope=scope,
+            loss_collection=None,
+            reduction=reduction)
+
+        loss = loss_class0 + loss_class1
+        tf.losses.add_loss(loss, loss_collection)
+
+        if add_summaries:
+            tf.summary.scalar('density_ratio_loss', loss)
+
+
+    return loss
+
+def density_ratio_generator_loss(
+    discriminator_outputs,
+    weights=1.0,
+    label_smoothing=0,
+    scope=None,
+    loss_collection=tf.GraphKeys.LOSSES,
+    reduction=tf.losses.Reduction.SUM_BY_NONZERO_WEIGHTS,
+    add_summaries=False):
+    with tf.name_scope(
+        scope,
+        'generator_density_ratio_loss',
+        [discriminator_outputs, weights, label_smoothing]) as scope:
+        loss = density_ratio_loss(
+            discriminator_outputs,
+            weights,
+            label_smoothing,
+            scope,
+            loss_collection,
+            reduction,
+            add_summaries=False)
+
+        if add_summaries:
+            tf.summary.scalar('generator_density_ratio_loss', loss)
+
+    return loss
+
+def density_ratio_encoder_loss(
+    discriminator_outputs,
+    weights=1.0,
+    label_smoothing=0,
+    scope=None,
+    loss_collection=tf.GraphKeys.LOSSES,
+    reduction=tf.losses.Reduction.SUM_BY_NONZERO_WEIGHTS,
+    add_summaries=False):
+    with tf.name_scope(
+        scope,
+        'encoder_density_ratio_loss',
+        [discriminator_outputs, weights, label_smoothing]) as scope:
+        loss = density_ratio_loss(
+            discriminator_outputs,
+            weights,
+            label_smoothing,
+            scope,
+            loss_collection,
+            reduction,
+            add_summaries=False)
+
+        if add_summaries:
+            tf.summary.scalar('encoder_density_ratio_loss', loss)
+
+    return loss
+
+def least_absolute_loss(
+    data,
+    reconstructed_data,
+    weights=1.0,
+    scope=None,
+    loss_collection=tf.GraphKeys.LOSSES,
+    reduction=tf.losses.Reduction.SUM_BY_NONZERO_WEIGHTS,
+    add_summaries=False):
+    with tf.name_scope(
+        scope,
+        'least_absolute_loss',
+        [data, reconstructed_data, weights]) as scope:
+        loss = tf.losses.absolute_difference(
+            data,
+            reconstructed_data,
+            weights,
+            scope,
+            loss_collection,
+            reduction)
+
+        if add_summaries:
+            tf.summary.scalar('least_absolute_loss', loss)
+
+    return loss
+
+def alphagan_encoder_loss(
+    discriminator_gen_outputs,
+    data,
+    reconstructed_data,
+    encoder_loss_fn=density_ratio_encoder_loss,
+    reconstruction_loss_fn=least_absolute_loss,
+    reconstruction_loss_weight=1.0,
+    add_summaries=False):
+    loss_on_enc = encoder_loss_fn(discriminator_gen_outputs)
+    rec_loss = reconstruction_loss_fn(
+        data, reconstructed_data, reconstruction_loss_weight)
+
+    loss = loss_on_enc + rec_loss
+
+    if add_summaries:
+        tf.summary.scalar('alphagan_encoder_loss', loss)
+
+    return loss
+
+def alphagan_generator_loss(
+    discriminator_gen_outputs,
+    discriminator_rec_outputs,
+    data,
+    reconstructed_data,
+    generator_loss_fn=density_ratio_generator_loss,
+    reconstruction_loss_fn=least_absolute_loss,
+    reconstruction_loss_weight=1.0,
+    add_summaries=False):
+    loss_on_gen = generator_loss_fn(discriminator_gen_outputs)
+
+    loss_on_rec = generator_loss_fn(discriminator_rec_outputs)
+
+    rec_loss = reconstruction_loss_fn(
+        data, reconstructed_data, reconstruction_loss_weight)
+
+    loss = rec_loss + loss_on_rec + loss_on_gen
+
+    if add_summaries:
+        tf.summary.scalar('alphagan_generator_loss', loss)
+
+    return loss
+
+def alphagan_discriminator_loss(
+    discriminator_real_outputs,
+    discriminator_gen_outputs,
+    discriminator_rec_outputs,
+    discriminator_fn=modified_discriminator_loss,
+    add_summaries=False):
+    dis_loss = discriminator_fn(
+        discriminator_real_outputs, discriminator_gen_outputs)
+
+    loss_on_rec = tf.losses.sigmoid_cross_entropy(
+        tf.zeros_like(discriminator_rec_outputs),
+        discriminator_rec_outputs)
+
+    loss = dis_loss + loss_on_rec
+
+    if add_summaries:
+        tf.summary.scalar('alphagan_discriminator_loss', loss)
+
+    return loss
+
+def alphagan_code_discriminator_loss(
+    discriminator_real_outputs,
+    discriminator_gen_outputs,
+    add_summaries=False):
+    loss = modified_discriminator_loss(
+        discriminator_real_outputs,
+        discriminator_gen_outputs,
+        scope='alphagan_code_discriminator_loss')
+
+    if add_summaries:
+        tf.summary.scalar('alphagan_code_discriminator_loss', loss)
+
+    return loss
---
+++ alpha-gan/mnist_run.py
@@ -0,0 +1,269 @@
+import argparse
+import functools
+import os
+import time
+
+from matplotlib import cm
+from matplotlib import figure
+from matplotlib.backends import backend_agg
+import numpy as np
+import tensorflow as tf
+
+from codebase.alphagan import make_generator, make_discriminator
+import codebase.losses as loss
+from codebase import mnist_dataset
+
+from tensorflow.contrib.learn.python.learn.datasets import mnist
+
+IMAGE_SHAPE = [28, 28, 1]
+
+def generator_fn(z):
+    """
+    Args:
+        z: A `float`-like `Tensor` [batch_size, 1, 1, latent_size]
+    Returns:
+        net:
+    """
+    net = z
+    net = tf.layers.conv2d_transpose(
+        net, 64, kernel_size=5, strides=2, activation=tf.nn.relu)
+    net = tf.layers.conv2d_transpose(
+        net, 32, kernel_size=5, strides=2, activation=tf.nn.relu)
+    net = tf.layers.conv2d_transpose(
+        net, 1, kernel_size=4, strides=2, activation=tf.nn.tanh)
+
+    return net
+
+def discriminator_fn(images):
+    net = images
+    net = tf.layers.conv2d(
+        net, 32, kernel_size=4, strides=2, activation=tf.nn.relu)
+    net = tf.layers.conv2d(
+        net, 64, kernel_size=5, strides=2, activation=tf.nn.relu)
+    net = tf.layers.conv2d(net, 1, kernel_size=5, strides=2)
+
+    return net
+
+def encoder_fn(images, latent_size):
+    net = images
+    net = tf.layers.conv2d(
+        net, 32, kernel_size=4, strides=2, activation=tf.nn.relu)
+    net = tf.layers.conv2d(
+        net, 64, kernel_size=5, strides=2, activation=tf.nn.relu)
+    net = tf.layers.conv2d(net, latent_size, kernel_size=5, strides=2)
+
+    return net
+
+def code_discriminator_fn(z):
+    net = z
+    net = tf.layers.dense(net, 1)
+
+    return net
+
+
+def save_imgs(x, fname):
+    n = x.shape[0]
+    fig = figure.Figure(figsize=(n, 1), frameon=False)
+    canvas = backend_agg.FigureCanvasAgg(fig)
+    for i in range(n):
+        ax = fig.add_subplot(1, n, i+1)
+        ax.imshow(
+            x[i].squeeze(),
+            interpolation='none',
+            cmap=cm.get_cmap('binary'))
+        ax.axis('off')
+    canvas.print_figure(fname, format='png')
+    print('saved {:s}'.format(fname))
+
+def visualize_training(
+    images, reconstructed_images, log_dir, prefix, viz_num=5):
+    save_imgs(
+        images[:viz_num],
+        os.path.join(log_dir, '{}_inputs.png'.format(prefix)))
+    save_imgs(
+        reconstructed_images[:viz_num],
+        os.path.join(log_dir, '{}_reconstructions.png'.format(prefix)))
+
+def build_input_pipline(training_dataset, batch_size, heldout_size):
+    """Build an Iterator switching between train and heldout data."""
+    # Build an iterator over training batches
+    training_batches = training_dataset.repeat().batch(batch_size)
+    training_iterator = training_batches.make_one_shot_iterator()
+
+    # Build a iterator over the heldout set with batch size=heldout_size
+    heldout_frozen = training_dataset.take(heldout_size).repeat().batch(heldout_size)
+    heldout_iterator = heldout_frozen.make_one_shot_iterator()
+
+    handle = tf.placeholder(tf.string, shape=[])
+    feedable_iterator = tf.data.Iterator.from_string_handle(
+        handle, training_batches.output_types, training_batches.output_shapes)
+    images, labels = feedable_iterator.get_next()
+
+    return images, labels, handle, training_iterator, heldout_iterator
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Alpha GAN example: MNIST')
+    parser.add_argument('--data_dir', default=os.path.join(os.getcwd(), 'data/mnist'))
+    parser.add_argument('--model_dir',
+                        default=os.path.join('checkpoint/mnist', '2018-06-05-1547'),
+                        help='Directory to put the model')
+
+    # model parameters
+    parser.add_argument('--latent_size', type=int, default=2)
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--heldout_size', type=int, default=10000)
+
+    # train parameters
+    parser.add_argument('--learning_rate', type=float, default=0.01,
+                        help='Initial learning rate')
+    parser.add_argument('--max_steps', type=int, default=100)
+    parser.add_argument('--viz_steps', type=int, default=500)
+
+
+    args = parser.parse_args()
+
+    if tf.gfile.Exists(args.model_dir):
+        tf.logging.warn('Deleting old log directory at {}'.format(args.model_dir))
+        tf.gfile.DeleteRecursively(args.model_dir)
+    tf.gfile.MakeDirs(args.model_dir)
+
+    mnist_data = mnist_dataset.train(args.data_dir)
+
+    with tf.Graph().as_default():
+        (images, _, handle,
+         training_iterator, heldout_iterator) = build_input_pipline(
+             mnist_data, args.batch_size, args.heldout_size)
+
+        images = tf.reshape(images, shape=[-1] + IMAGE_SHAPE)
+        noise  = tf.random_normal([args.batch_size, 1, 1, args.latent_size])
+
+        # model generator
+        generator = make_generator(generator_fn, noise)
+        discriminator = make_discriminator(
+            discriminator_fn, generator.outputs, images)
+
+        # model encoder
+        encoder = make_generator(
+            functools.partial(encoder_fn, latent_size=args.latent_size),
+            images, scope='encoder')
+        code_discriminator = make_discriminator(
+            code_discriminator_fn, encoder.outputs,
+            noise, scope='code_discriminator')
+
+        with tf.variable_scope(generator.scope, reuse=True):
+            reconstructed_data = generator.fn(encoder.outputs)
+
+        with tf.variable_scope(discriminator.scope, reuse=True):
+            d_rec_outputs = discriminator.fn(reconstructed_data)
+
+        g_loss = loss.alphagan_generator_loss(
+            discriminator.gen_outputs, d_rec_outputs,
+            images, reconstructed_data, add_summaries=True)
+
+        d_loss = loss.alphagan_discriminator_loss(
+            discriminator.real_outputs, discriminator.gen_outputs,
+            d_rec_outputs, add_summaries=True)
+
+        e_loss = loss.alphagan_encoder_loss(
+            code_discriminator.gen_outputs,
+            images, reconstructed_data, add_summaries=True)
+
+        code_d_loss = loss.alphagan_code_discriminator_loss(
+            code_discriminator.real_outputs,
+            code_discriminator.gen_outputs,
+            add_summaries=True)
+
+        g_optimizer = tf.train.AdamOptimizer(args.learning_rate)
+        g_train_op  = g_optimizer.minimize(
+            g_loss, var_list=generator.variables)
+
+        d_optimizer = tf.train.AdamOptimizer(args.learning_rate)
+        d_train_op  = d_optimizer.minimize(
+            d_loss, var_list=discriminator.variables)
+
+        e_optimizer = tf.train.AdamOptimizer(args.learning_rate)
+        e_train_op  = e_optimizer.minimize(e_loss, var_list=encoder.variables)
+
+        code_d_optimizer = tf.train.AdamOptimizer(args.learning_rate)
+        code_d_train_op  = code_d_optimizer.minimize(
+            code_d_loss, var_list=code_discriminator.variables)
+
+        # summary
+        tf.summary.image('images', images, max_outputs=1)
+        tf.summary.image('generated_images', generator.outputs, max_outputs=1)
+        tf.summary.image('reconstructed_images', reconstructed_data, max_outputs=1)
+        summary = tf.summary.merge_all()
+
+        init    = tf.global_variables_initializer()
+        saver   = tf.train.Saver()
+        with tf.Session() as sess:
+            summary_writer = tf.summary.FileWriter(args.model_dir, sess.graph)
+            sess.run(init)
+
+            # Run the training loop
+            train_handle = sess.run(training_iterator.string_handle())
+            heldout_handle = sess.run(heldout_iterator.string_handle())
+            for step in range(args.max_steps):
+                start_time = time.time()
+
+                _, e_loss_value = sess.run(
+                    [e_train_op, e_loss],
+                    feed_dict={handle: train_handle})
+
+                _, g_loss_value = sess.run(
+                    [g_train_op, g_loss],
+                    feed_dict={handle: train_handle})
+
+                _, d_loss_value = sess.run(
+                    [d_train_op, d_loss],
+                    feed_dict={handle: train_handle})
+
+                _, code_d_loss_value = sess.run(
+                    [code_d_train_op, code_d_loss],
+                    feed_dict={handle: train_handle})
+
+                duration = time.time() - start_time
+
+                if step % 100 == 0:
+                    print('Step: {:>3d} Discriminator Loss: {:.3f} '
+                          'Code Discriminator Loss: {:.3f} ({:.3f} sec)'.format(
+                              step, d_loss_value, code_d_loss_value, duration))
+
+                    summary_str = sess.run(summary, feed_dict={handle: train_handle})
+                    summary_writer.add_summary(summary_str, step)
+                    summary_writer.flush()
+
+                if (step + 1) % args.viz_steps == 0 or (step + 1) == args.max_steps:
+                    checkpoint_file = os.path.join(args.model_dir, 'model.ckpt')
+                    saver.save(sess, checkpoint_file, global_step=step)
+
+                    # generator
+                    generated_value = sess.run([generator.outputs])
+                    save_imgs(
+                        generated_value[0][:5],
+                        os.path.join(args.model_dir, 'step{:05d}_generator.png'.format(step)))
+
+                    # training data
+                    images_value, reconstructions_value = sess.run(
+                        [images, reconstructed_data],
+                        feed_dict={handle: train_handle})
+                    visualize_training(
+                        images_value,
+                        reconstructions_value,
+                        log_dir=args.model_dir,
+                        prefix='step{:05d}_train'.format(step))
+
+                    # validation data
+                    heldout_images_value, heldout_reconstructions_value = sess.run(
+                        [images, reconstructed_data],
+                        feed_dict={handle: heldout_handle})
+                    visualize_training(
+                        heldout_images_value,
+                        heldout_reconstructions_value,
+                        log_dir=args.model_dir,
+                        prefix='step{:05d}_validation'.format(step))
+
+
+if __name__ == '__main__':
+    main()
